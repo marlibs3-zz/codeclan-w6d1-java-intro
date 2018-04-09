343e6fc78aafb58bd2c3254a58348172d453e41a
martabeveridge <marta@beveridge.uk>
2018-04-09 11:07:47 +0100

    first java project commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args){
+        Bear myBear = new Bear("Yogi");
+        String bearName = myBear.getName();
+        System.out.println(bearName);
+    }
+}
